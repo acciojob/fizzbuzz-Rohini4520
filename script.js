67c94f0526dbd3d7db62ad4c4175ae997fbee9af
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-04-15 21:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let num;
+if(num %3){
+	console.log("Fizz");
+}else if(num% 5){
+	console.log("Buzz");
+}
+else if(num % 3   || num %5){
+	console.log("FizzBuzz");
+}
